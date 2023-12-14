e3894c4bdfd84a01401e98c800c0e4686abea0b4
ay-eonii <naayen@naver.com>
2023-12-14 17:41:19 +0900

    feat(WeekDay): 한글 추가

--- src/main/java/menu/domain/WeekDay.java
+++ src/main/java/menu/domain/WeekDay.java
@@ -3,17 +3,23 @@ package menu.domain;
 import java.util.StringJoiner;
 
 public enum WeekDay {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY;
+    MONDAY("월요일"),
+    TUESDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일");
+
+    private final String korean;
+
+    WeekDay(String korean) {
+        this.korean = korean;
+    }
 
     public static String printWeekDay() {
         StringJoiner stringJoiner = new StringJoiner(" | ", "[ ", " ]");
         stringJoiner.add("구분");
         for (WeekDay value : values()) {
-            stringJoiner.add(value.name());
+            stringJoiner.add(value.korean);
         }
         return stringJoiner.toString();
     }
